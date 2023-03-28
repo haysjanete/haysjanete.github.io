7f11e4f2928a9336b94723957c7b465e893a4778
haysjanete <74339100+haysjanete@users.noreply.github.com>
2023-03-28 15:56:59 -0500

    pushing all changes due to design  change

---
+++ src/WelcomeBtn.js
@@ -0,0 +1,35 @@
+function hideButton(id) {
+          const wbc = document.getElementById(id);
+          if (wbc.style.display == 'flex') {
+            wbc.style.display = "none";
+          } else {
+            wbc.style.display = "flex";
+          }
+        }
+
+ function openGlass(id) {
+          const gc = document.getElementById(id);
+          if (gc.style.display == "none") {
+            gc.style.display = "flex";
+          } else {
+            gc.style.display = "none";
+          }
+        }
+
+function glassBtn(id) {
+          const wbc= document.getElementById(id);
+          if (wbc.style.display == "none") {
+            wbc.style.display = "flex";
+          } else {
+            wbc.style.display = "none";
+          }
+        }
+
+function closeContainer(id) {
+          const cd = document.getElementById(id);
+          if (cd.style.display == "none") {
+            cd.style.display = "flex";
+          } else {
+            cd.style.display = "none";
+          }
+        }
